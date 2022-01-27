aa8ad92b2e2f712a384a1acdd6c4cfae8f32e020
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-01-27 17:18:08 +0900

    level01_10_10926

---
+++ baekjoon/level01_입출력과사칙연산/level01_10_10926/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String A = scanner.next();
+        // java에서는 입력값의 성질 고려해야 함!
+
+        System.out.println(A + "??!");
+
+        scanner.close();
+    }
+}
