c16a507c6940ed75dc97fdeea42589cece94612f
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-01-28 11:46:05 +0900

    level03_07~08

--- baekjoon/level03_for/level03_06_2742/Main.java
+++ baekjoon/level03_for/level03_06_2742/Main.java
@@ -14,20 +14,3 @@ public class Main {
         }
     }
 
-// import java.util.Scanner;
-
-// public class Main {
-//     public static void main(String[] args){
-//         Scanner in = new Scanner(System.in);
-
-//         int N = in.nextInt();
-//         in.close();
-
-//         int i = 1;
-
-//         while(i <= N){
-//             System.out.println(i);
-//             i++;
-//         }
-//     }
-// }
